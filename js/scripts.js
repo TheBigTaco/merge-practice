b5a3eebf17632c9dc92f07c7e845a84e68c3af55
Rane Fields <rane.fields@gmail.com>
2017-10-02 08:56:44 -0700

    Add pet data structure and adopt method

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 // DO SOME STUFF I GUESS
+function Pet(name) {
+  this.name = name;
+  this.isAdopted = false;
+  this.owner = "";
+}
+
+Pet.prototype.Adopt = function(newOwner) {
+  this.isAdopted = true;
+  this.owner = newOwner;
+}
